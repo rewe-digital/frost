b9680b8218d2345b0a2cc1f26173cf52a7170443
Stefan Neuhaus <stefan@stefanneuhaus.org>
2019-01-02 15:20:38 +0100

    Fix issues with killing/removing of containers when projectName is specified Extract start logic of DockerComposeTasks to parent class

--- src/main/groovy/org/rewedigital/frost/tasks/FrostComposePullTask.groovy
+++ src/main/groovy/org/rewedigital/frost/tasks/FrostComposePullTask.groovy
@@ -4,8 +4,6 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.rewedigital.frost.util.Util
 
-import static org.rewedigital.frost.FrostPluginExtension.EXTENSION_NAME
-
 class FrostComposePullTask extends DockerComposeTask {
 
     @OutputDirectory
@@ -16,10 +14,7 @@ class FrostComposePullTask extends DockerComposeTask {
     @TaskAction
     @Override
     def action() {
-        def cmd = ["/bin/sh"]
-        cmd << "-c"
-        cmd << "export TAG=${project.extensions[EXTENSION_NAME].sutTag} && ${EXECUTABLE} -f ${getComposeFile()} -f ${getComposeOverrideFile()} pull --ignore-pull-failures"
-
+        def cmd = dockerComposeCommand("pull --ignore-pull-failures")
         Util.executeSynchronously(cmd, "docker_compose_pull", getReportDirectory())
     }
 }
--- src/main/groovy/org/rewedigital/frost/tasks/FrostComposeUpTask.groovy
+++ src/main/groovy/org/rewedigital/frost/tasks/FrostComposeUpTask.groovy
@@ -4,8 +4,6 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.rewedigital.frost.util.Util
 
-import static org.rewedigital.frost.FrostPluginExtension.EXTENSION_NAME
-
 class FrostComposeUpTask extends DockerComposeTask {
 
     @OutputDirectory
@@ -16,20 +14,7 @@ class FrostComposeUpTask extends DockerComposeTask {
     @TaskAction
     @Override
     def action() {
-        def env = [
-                TAG: project.extensions[EXTENSION_NAME].sutTag
-        ]
-
-        if (project.extensions[EXTENSION_NAME].projectName) {
-            env << [COMPOSE_PROJECT_NAME: project.extensions[EXTENSION_NAME].projectName]
-        }
-
-        def envString = env.inject([]) { result, entry -> result << "${entry.key}=${entry.value}" }.join(" ")
-
-        def cmd = ["/bin/sh"]
-        cmd << "-c"
-        cmd << "export ${envString} && ${EXECUTABLE} -f ${getComposeFile()} -f ${getComposeOverrideFile()} up"
-
+        def cmd = dockerComposeCommand("up")
         Util.executeAsynchronously(cmd, "docker_compose_up", getReportDirectory())
     }
 }
